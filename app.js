cc0f3c0cbe195728d7030eb407b0ee2714ace715
zuhi-shaik <sahirazuhi@gmail.com>
2026-02-13 10:58:10 +0530

    Version update

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('DevOps Live Project - Version 1 🚀');
+    res.send('DevOps Live Project - Version 1 🚀');
 });
 
 app.listen(3000, () => {
-  console.log('Server running on port 3000');
+    console.log('Server running on port 8080');
 });
